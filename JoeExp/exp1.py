963cf595b5330368134febc7632711c21ed3fabc
ProgrammingJoe <joeczepil@gmail.com>
2016-01-10 12:05:46 -0800

    Set up file structure

---
+++ JoeExp/exp1.py
@@ -0,0 +1,18 @@
+import random
+
+def main():
+    while True:
+        greetings = ['hi', 'HI', 'Hi']
+        questionsoffeelings = ['How are you?', 'How are you doing?', 'How are you feeling?']
+        answersforfeelings = ['Good, yourself?', 'Shitty. :(']
+
+        userInput = input(">>> ")
+        if userInput in greetings:
+            print("Hello")
+        elif userInput in questionsoffeelings:
+            print(random.choice(answersforfeelings))
+        else:
+            print("I do not understand you.")
+
+if __name__ == '__main__':
+   main()
